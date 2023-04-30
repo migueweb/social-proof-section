752fc18b81f18860d398cc2a99e548f558d9042b
Miguel Amador <miguesdev@gmail.com>
2023-04-30 16:43:13 -0500

    style: add custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    colors: {
+      'very-dark-magenta': 'hsl(300, 43%, 22%)',
+      'soft-pink': 'hsl(333, 80%, 67%)',
+      'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
+      'light-grayish-magenta': 'hsl(300, 24%, 96%)',
+    },
+
+    fontSize: {
+      'base':['15px'],
+    },
+
+    extend: {
+      fontFamily: {
+        'league-spartan': ['League Spartan', 'sans-serif'],
+      },  
+    },
   },
   plugins: [],
 }
